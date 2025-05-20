288bea2dc1103a5ea8f1ddcd60779ab4a4f0c689
Lê Ngọc Phúc <147823652+ngphuc1503@users.noreply.github.com>
2025-05-20 10:50:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function handleResponse(accepted) {
+  if (accepted) {
+    alert("Thank you for subscribing!");
+    // Thêm logic gửi opt-in nếu cần
+  } else {
+    alert("Maybe next time.");
+  }
+}
